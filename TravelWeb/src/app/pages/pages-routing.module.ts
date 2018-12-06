ceda7900f3df8da3f81c24fc84e9d64ce83a7d1e
BuiNguyenHoangAnh <hoanganhbui099@gmail.com>
2018-12-06 14:06:16 +0700

    update profile.component

--- TravelWeb/src/app/pages/pages-routing.module.ts
+++ TravelWeb/src/app/pages/pages-routing.module.ts
@@ -4,13 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ReviewComponent } from './review/review.component';
 import { OverviewComponent } from './overview/overview.component';
 import { PagesComponent } from './pages.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [{
   path: '',
   component: PagesComponent,
   children: [
-    // { path: 'review', component: ReviewComponent },
-    { path: '', component: ReviewComponent },
+    { path: 'review', component: ReviewComponent },
+    { path: '', component: ProfileComponent },
     { path: 'overview', component: OverviewComponent }
   ]
 }];
