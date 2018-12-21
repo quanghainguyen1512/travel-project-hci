422a0db3e3568ec5ab5c84d2ce262fdc86cebc42
BuiNguyenHoangAnh <hoanganhbui099@gmail.com>
2018-12-21 15:30:21 +0700

    add sign in template

--- TravelWeb/src/app/template/header/header.component.ts
+++ TravelWeb/src/app/template/header/header.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-header',
@@ -6,13 +12,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  noti : number = 0;
 
-  constructor() { }
+  resultFromDialog: string;
+  isSignIn = false;
+
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
   }
 
   public showSignIn() {
-    console.log("Popup");
+    const dialogRef = this.dialog.open(SignInComponent, {
+      // signInAction = false;
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.isSignIn = result;
+    });
   }
+}
+
+@Component({
+  selector: 'sign-in',
+  templateUrl: './sign-in.html',
+  styleUrls: ['./sign-in.css']
+})
+export class SignInComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<SignInComponent>,
+    @Inject(MAT_DIALOG_DATA) public isSignIn: boolean) {}
 }
--- TravelWeb/src/app/template/template.module.ts
+++ TravelWeb/src/app/template/template.module.ts
@@ -2,24 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent, SignInComponent } from './header/header.component';
 
 import { RouterModule } from '@angular/router';
-import { MatBadgeModule, MatIconModule } from '@angular/material';
+import { MatBadgeModule, MatIconModule, MatDialogModule } from '@angular/material';
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent],
+  declarations: [FooterComponent, HeaderComponent, SignInComponent],
   imports: [
     CommonModule,
     RouterModule,
     MatBadgeModule,
     MatIconModule,
+    MatDialogModule,
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
     MatBadgeModule,
     MatIconModule,
+    MatDialogModule,
+  ],
+  entryComponents: [
+    SignInComponent,
   ]
 })
 export class TemplateModule { }
