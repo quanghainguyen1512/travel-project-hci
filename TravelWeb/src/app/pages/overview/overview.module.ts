60e6c08b5bd0041dc22dbd9b79f7be978b5faa2d
BuiNguyenHoangAnh <hoanganhbui099@gmail.com>
2018-12-21 11:08:03 +0700

    update overview component

--- TravelWeb/src/app/pages/overview/overview.module.ts
+++ TravelWeb/src/app/pages/overview/overview.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OverviewComponent } from './overview.component';
+import { MatTabsModule } from '@angular/material';
 
 @NgModule({
   declarations: [OverviewComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTabsModule,
   ],
   exports: [
-    OverviewComponent
+    OverviewComponent,
+    MatTabsModule,
   ]
 })
 export class OverviewModule { }
