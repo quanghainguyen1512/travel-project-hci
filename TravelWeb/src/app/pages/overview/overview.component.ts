9949e5dae225ae0b8f2c2b67393c038f888ab6d6
BuiNguyenHoangAnh <hoanganhbui099@gmail.com>
2018-12-22 20:48:42 +0700

    update home page

--- TravelWeb/src/app/pages/overview/overview.component.ts
+++ TravelWeb/src/app/pages/overview/overview.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {PageEvent} from '@angular/material';
+import { NguCarousel, NguCarouselConfig, NguCarouselStore } from '@ngu/carousel';
 
 export interface Article {
   image: string;
@@ -14,6 +15,12 @@ export interface Article {
   styleUrls: ['./overview.component.css']
 })
 export class OverviewComponent implements OnInit {
+
+  // private carouselToken: string;
+ 
+  // public carouselTileItems: Array<any>;
+  // public carouselTile: NguCarouselConfig;
+  // @ViewChild('carousel') carousel: NguCarousel<any>;
 
   listArticle : Article = 
     {
@@ -32,6 +39,20 @@ export class OverviewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // this.carouselTileItems = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+ 
+    // this.carouselTile = {
+    //   grid: {xs: 2, sm: 3, md: 3, lg: 5, all: 0},
+    //   slide: 2,
+    //   speed: 400,
+    //   animation: 'lazy',
+    //   point: {
+    //     visible: true
+    //   },
+    //   load: 2,
+    //   touch: true,
+    //   easing: 'ease'
+    // }
   }
 
   scroll(el) {
@@ -43,4 +64,26 @@ export class OverviewComponent implements OnInit {
     // let secondCut = firstCut + e.pageSize;
     // this.listReview = this.reviewDetail.slice(firstCut, secondCut);
   }
+
+  // initDataFn(key: NguCarouselStore) {
+  //   this.carouselToken = key.token;
+  // }
+ 
+  // resetFn() {
+  //   this.carousel.reset(this.carouselToken);
+  // }
+ 
+  // moveToSlide() {
+  //   this.carousel.moveToSlide(this.carouselToken, 2, false);
+  // }
+ 
+  // public carouselTileLoad(evt: any) {
+ 
+  //   const len = this.carouselTileItems.length
+  //   if (len <= 30) {
+  //     for (let i = len; i < len + 10; i++) {
+  //       this.carouselTileItems.push(i);
+  //     }
+  //   }
+  // }
 }
--- TravelWeb/src/app/pages/overview/overview.module.ts
+++ TravelWeb/src/app/pages/overview/overview.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { OverviewComponent } from './overview.component';
 import { MatTabsModule, MatPaginatorModule } from '@angular/material';
+import { NguCarouselModule } from '@ngu/carousel';
 
 @NgModule({
   declarations: [OverviewComponent],
@@ -10,11 +11,13 @@ import { MatTabsModule, MatPaginatorModule } from '@angular/material';
     CommonModule,
     MatTabsModule,
     MatPaginatorModule,
+    NguCarouselModule
   ],
   exports: [
     OverviewComponent,
     MatTabsModule,
     MatPaginatorModule,
+    NguCarouselModule,
   ]
 })
 export class OverviewModule { }
