f6f011f0d106f8f45c0e6b26372e900133c2003f
BuiNguyenHoangAnh <hoanganhbui099@gmail.com>
2018-12-22 22:36:52 +0700

    update profile component

--- TravelWeb/src/app/pages/profile/profile.component.ts
+++ TravelWeb/src/app/pages/profile/profile.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  follower: number = 89;
+  isFollow: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  follow() {
+    this.follower+=1;
+    this.isFollow = true;
+  }
+
+  unfollow() {
+    this.follower-=1;
+    this.isFollow = false;
   }
 
 }
