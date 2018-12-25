7fef379ff7e3236096ce376a9c4763b300d6cf49
AnhPmcl2015 <phananh.pmcl2015@gmail.com>
2018-12-26 00:55:30 +0700

    Cập nhật profile

--- TravelWeb/src/app/pages/profile/profile.component.ts
+++ TravelWeb/src/app/pages/profile/profile.component.ts
@@ -3,25 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
 
   numOfFollower: number = 89;
   isFollow: boolean = false;
-
-  name = 'Johnny';
-  numOfPost = 'Number of posts: ';
-  follower = 'Follower: ';
-  following = 'Following: ';
-  btnFollow = '+ Follow';
-  btnUnfollow = '- Unfollow';
-  joined = 'Joined: ';
-  joinTime = 'March 14 2012';
-  describe = 'A personal diary of all the adventures in the rockies and travels to explore new sights.';
-  title = 'Hồ Con Rùa';
-  overviewArticle = 'Ròng rã suốt 3 thế kỷ từ thời Pháp thuộc, trước và sau 1975, địa danh Hồ con Rùa được xem là một công trình văn hóa, lịch sử đã từng chứng kiến vô số sự kiện sôi nổi của lịch sử nước ta...';
-  
 
   constructor() { }
 
