9d59e8256052a92de2839ffc012e3a5b4c17bf54
AnhPmcl2015 <phananh.pmcl2015@gmail.com>
2019-01-05 14:08:03 +0700

    cn

--- TravelWeb/src/app/template/header-transparent/header-transparent.module.ts
+++ TravelWeb/src/app/template/header-transparent/header-transparent.module.ts
@@ -1,14 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderTransparentComponent } from './header-transparent.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeaderTransparentComponent, SignInComponent, SignOutComponent} from './header-transparent.component';
+import { RouterModule } from '@angular/router';
+import { MatBadgeModule, MatIconModule, MatDialogModule, MatMenuModule, MatButtonModule, MatAutocompleteModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [HeaderTransparentComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    HeaderTransparentComponent,
-  ]
+    declarations: [HeaderTransparentComponent, SignInComponent, SignOutComponent],
+    imports: [
+
+        CommonModule, 
+        RouterModule,
+        MatBadgeModule,
+        MatIconModule,
+        MatDialogModule,
+        MatMenuModule,
+        MatButtonModule,
+        MatAutocompleteModule,
+        ReactiveFormsModule
+    ],
+    exports: [HeaderTransparentComponent]
 })
-export class HeaderTransparentModule { }
+export class HeaderTransparentModule {}
