219de0ce2559217178279621f1890a523a98455a
Hai Nguyen <quanghai.ng1512@gmail.com>
2019-01-09 09:30:22 +0700

    Remove DevMenuTrigger

--- TravelApp/index.js
+++ TravelApp/index.js
@@ -1,10 +1,8 @@
 /** @format */
 
 import { AppRegistry } from 'react-native';
-import React from 'react';
 import App from './App';
 import { name as appName } from './app.json';
-import DevMenuTrigger from './DevMenuTrigger';
 
 // AppRegistry.registerComponent(appName, () => <DevMenuTrigger><App /></DevMenuTrigger>);
 AppRegistry.registerComponent(appName, () => App);
