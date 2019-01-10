7318543b86537fc286fde19775b39b7db3b66df8
Hai Nguyen <quanghai.ng1512@gmail.com>
2019-01-10 13:15:45 +0700

    Update packages, images, data

---
+++ TravelApp/data/Explore.Data.js
@@ -0,0 +1,46 @@
+export const topContributors = [
+    {
+        name: 'Hàn Hằng',
+        user: 'hanhang',
+        topIn: 'Ho Chi Minh City'
+    },
+    {
+        name: 'Lucy',
+        user: 'kieutrinh',
+        topIn: 'Singapore'
+    },
+    {
+        name: 'NoName',
+        user: 'khanhvan',
+        topIn: 'Bangkok'
+    },
+    {
+        name: 'NoName',
+        user: 'khanhvan',
+        topIn: 'Bangkok'
+    },
+    {
+        name: 'NoName',
+        user: 'khanhvan',
+        topIn: 'Bangkok'
+    },
+];
+
+export const hotDestinations = [
+    {
+        dest: 'Bangkok',
+        img: 'bangkok'
+    },
+    {
+        dest: 'Singapore',
+        img: 'singapore'
+    },
+    {
+        dest: 'Phuket',
+        img: 'phuket'
+    },
+    {
+        dest: 'Pattaya',
+        img: 'pattaya'
+    },
+];
